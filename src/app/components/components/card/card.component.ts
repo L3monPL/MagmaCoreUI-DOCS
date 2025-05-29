64bcabba160e23c50e23af608ddc4f179307d1d9
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2025-05-29 14:51:08 +0200

    import .

--- src/app/components/components/card/card.component.ts
+++ src/app/components/components/card/card.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MagmaCardModule } from 'magma-core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MagmaCardModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
 
+  codeImport = `import { MagmaCardModule } from 'magma-core';`
+
+
+  copyCode(code: any) {
+    navigator.clipboard.writeText(code).then(() => {
+      alert('Skopiowano kod!');
+    });
+  }
 }
