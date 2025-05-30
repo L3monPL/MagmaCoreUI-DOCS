491357c97d6d6567eddeb5f9c3cbbbd3443e0859
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2025-05-30 14:23:40 +0200

    ./

---
+++ docs/browser/chunk-26OEDNPY.js
@@ -0,0 +1,8 @@
+import{B as C,f as s,k as c,w as g,x as f,y as u}from"./chunk-NFCC2EWM.js";import"./chunk-PUAS7KUQ.js";import{$ as p,Na as l,Ua as e,Va as t,Wa as a,eb as i,mb as d,ya as o}from"./chunk-WUAR7S4Q.js";var I=(()=>{class m{constructor(){this.codeImport="import { MagmaTimelineModule } from 'magma-core';",this.codeTimelineExamp=`<magma-timeline>
+  <magma-timeline-item>
+    Item 1
+  </magma-timeline-item>
+  <magma-timeline-item>
+    Item 2
+  </magma-timeline-item>
+</magma-timeline>`}static{this.\u0275fac=function(n){return new(n||m)}}static{this.\u0275cmp=p({type:m,selectors:[["app-timeline"]],standalone:!0,features:[d],decls:19,vars:2,consts:[[1,"font-base-section"],[1,"description-section"],[3,"code"]],template:function(n,r){n&1&&(e(0,"h2",0),i(1,"Timeline is a UI component for visualizing time-based events"),t(),e(2,"p",1),i(3,"Timeline"),t(),a(4,"br"),e(5,"h3"),i(6,"Import"),t(),a(7,"app-code-template",2)(8,"br"),e(9,"h3"),i(10,"Basic"),t(),e(11,"magma-card")(12,"magma-timeline")(13,"magma-timeline-item"),i(14," Item 1 "),t(),e(15,"magma-timeline-item"),i(16," Item 2 "),t()()(),a(17,"br")(18,"app-code-template",2)),n&2&&(o(7),l("code",r.codeImport),o(11),l("code",r.codeTimelineExamp))},dependencies:[c,s,C,u,f,g],styles:["[_nghost-%COMP%]{flex:1;overflow-y:auto;padding-right:1rem;margin-right:-1rem}"]})}}return m})();export{I as TimelineComponent};
